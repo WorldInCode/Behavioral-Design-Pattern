45948232867514a9b642a2727971c8216d69dcdd
WorldInCode <8.8.15.2002.kmm@gmail.com>
2024-12-16 17:30:37 +0100

    Update CoffeeMachine.java

--- CoffeeMachine.java
+++ CoffeeMachine.java
@@ -1,1 +1,28 @@
+abstract class CoffeeMachine {
+    // Template method
+    public final void prepareCoffee() {
+        heatWater();
+        brewCoffee();
+        pourInCup();
+        addCondiments();
+        serve();
+    }
+
+    private void heatWater() {
+        System.out.println("Heating water...");
+    }
+
+    private void pourInCup() {
+        System.out.println("Pouring coffee into cup...");
+    }
+
+    private void serve() {
+        System.out.println("Serving the coffee!");
+    }
+
+    // Abstract methods to be implemented by subclasses
+    protected abstract void brewCoffee();
+
+    protected abstract void addCondiments();
+}
 
