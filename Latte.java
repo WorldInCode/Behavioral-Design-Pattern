47661e1c2a8a9507c082fc4ac5e8e36e88393e02
WorldInCode <8.8.15.2002.kmm@gmail.com>
2024-12-16 17:31:33 +0100

    Update Latte.java

--- Latte.java
+++ Latte.java
@@ -1,1 +1,12 @@
+class Latte extends CoffeeMachine {
+    @Override
+    protected void brewCoffee() {
+        System.out.println("Brewing coffee for a Latte...");
+    }
+
+    @Override
+    protected void addCondiments() {
+        System.out.println("Adding steamed milk...");
+    }
+}
 
