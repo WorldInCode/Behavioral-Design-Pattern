317068fb81dcc5f6ab91808e149e30e2cc8a25a3
WorldInCode <8.8.15.2002.kmm@gmail.com>
2024-12-16 17:31:52 +0100

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,1 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Preparing Espresso:");
+        CoffeeMachine espresso = new Espresso();
+        espresso.prepareCoffee();
+
+        System.out.println("\nPreparing Latte:");
+        CoffeeMachine latte = new Latte();
+        latte.prepareCoffee();
+    }
+}
 
