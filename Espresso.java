6388ef02ac193d425827f95ed4c1fa9202366f24
WorldInCode <8.8.15.2002.kmm@gmail.com>
2024-12-16 17:31:10 +0100

    Update Espresso.java

--- Espresso.java
+++ Espresso.java
@@ -1,1 +1,12 @@
+class Espresso extends CoffeeMachine {
+    @Override
+    protected void brewCoffee() {
+        System.out.println("Brewing a strong shot of espresso...");
+    }
+
+    @Override
+    protected void addCondiments() {
+        System.out.println("No condiments needed for Espresso.");
+    }
+}
 
